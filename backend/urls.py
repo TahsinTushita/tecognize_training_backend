c6e5fc16052c983b9cf3a5b62ab239493c9f08ea
TushitaTahsin <kazi26@student.sust.edu>
2022-01-22 00:31:47 +0600

    Changed urls

--- backend/urls.py
+++ backend/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url
 from backend import views
 
 urlpatterns = [
-    url(r"^api/users$", views.user_list),
-    url(r"^api/instructors$", views.instructor_list),
-    url(r"^api/customers$", views.customer_list),
-    url(r"^api/categories$", views.category_list),
-    url(r"^api/courses$", views.course_list),
-    url(r"^api/gallery$", views.gallery_list),
-    url(r"^api/blogs$", views.blog_list),
-    url(r"^api/reviews$", views.review_list),
-    url(r"^api/image/([a-zA-Z0-9_]+\.(?:jpg|gif|png|jpeg))$", views.getImage),
+    url(r"^backend/api/users$", views.user_list),
+    url(r"^backend/api/instructors$", views.instructor_list),
+    url(r"^backend/api/customers$", views.customer_list),
+    url(r"^backend/api/categories$", views.category_list),
+    url(r"^backend/api/courses$", views.course_list),
+    url(r"^backend/api/gallery$", views.gallery_list),
+    url(r"^backend/api/blogs$", views.blog_list),
+    url(r"^backend/api/reviews$", views.review_list),
+    url(r"^backend/api/image/([a-zA-Z0-9_]+\.(?:jpg|gif|png|jpeg))$", views.getImage),
 ]
